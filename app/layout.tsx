bdbfccb76307f4a689abb25acb89db89bfd24d29
Nitish Pandit <appgurunitish@gmail.com>
2025-01-24 13:43:04 +0530

    setup root layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,20 @@ import localFont from "next/font/local";
 
 const ibmPlexSans = localFont({
   src: [
-    { path: "/font/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
-    { path: "/font/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
-    { path: "/font/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "/font/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    {
+      path: "./fonts/IBMPlexSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    { path: "./fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
   ],
 });
 
 const bebasNeue = localFont({
   src: [
-    { path: "/font/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
   ],
   variable: "--bebas-neue",
 });
