9f0d98c3bb1716349f51930baff34bfb3ee8834e
Nitish Pandit <appgurunitish@gmail.com>
2025-01-27 14:16:56 +0530

    implement the auth UI form of signIn and signUp

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validations";
 import React from "react";
 
-const SignIn = () => {
-  return <div>SignIn</div>;
-};
+const SignIn = () => (
+  <AuthForm
+    type="SIGN_IN"
+    schema={signInSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+    }}
+    onSubmit={(data) => {
+      console.log(data);
+    }}
+  />
+);
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import { signUpSchema } from "@/lib/validations";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthForm
+      type="SIGN_UP"
+      schema={signUpSchema}
+      defaultValues={{
+        fullName: "",
+        email: "",
+        universityId: 0,
+        password: "",
+        universityCard: "",
+      }}
+      onSubmit={(data) => {
+        console.log(data);
+      }}
+    />
+  );
 };
 
 export default SignUp;
